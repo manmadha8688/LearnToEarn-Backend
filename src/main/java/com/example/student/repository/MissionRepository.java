e6b5730208f92d1c8214b8c5c9cb5ad09c015756
Manmadha Jayamangala <manmadha.jayamangala@cloudfuze.com>
2026-06-08 17:55:09 +0530

    code gym and quations

--- src/main/java/com/example/student/repository/MissionRepository.java
+++ src/main/java/com/example/student/repository/MissionRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface MissionRepository extends MongoRepository<Mission, String> {
     List<Mission> findByPublishedTrueOrderByOrderIndexAsc();
+    List<Mission> findAllByOrderByOrderIndexAsc();
 }
